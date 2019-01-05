8ed9fb1d8fe030c8f9d3a38002eebcd281039663
jacobkurian30 <jacobkurian30@gmail.com>
2019-01-05 13:23:14 -0500

    refactor

--- Lab_1_GUI_Pixelation/GUI_Pixelation/src/ImagePixelation.java
+++ Lab_1_GUI_Pixelation/GUI_Pixelation/src/ImagePixelation.java
@@ -1,67 +1,59 @@
 // Jacob Kurian
 // 09/03/2015
 // Lab 1
-
 import java.awt.Color;
 import simplegui.*;
 
 public class ImagePixelation {
 
     public static void main(String[] args) {
-        DrwImage im = new DrwImage("land.jpg");
-        SimpleGUI sg = new SimpleGUI(im.getWidth(), im.getHeight(), false);
+        DrwImage image = new DrwImage("land.jpg");
+        SimpleGUI simpleGui = new SimpleGUI(image.getWidth(), image.getHeight(), false);
 // getting width and height of the image
-        int width = im.getWidth();
-        int height = im.getHeight();
-        sg.drawImage(im, 0, 0, width, height, null);
+        int imageWidth = image.getWidth();
+        int imageHeight = image.getHeight();
+        simpleGui.drawImage(image, 0, 0, imageWidth, imageHeight, null);
 
         int gridSize = 16;
         // to remove the extra boxes at the last corner
-        sg.eraseAllDrawables();
-        pixImage(im, sg, gridSize);
-           
+        simpleGui.eraseAllDrawables();
+        pixelateImage(image, simpleGui, gridSize);
+
     }
 
-
-    
 // Method to get Pixelate image
-
-    public static void pixImage(DrwImage im, SimpleGUI sg, int gridSize) {
-        for (int row = 0; row < im.getHeight() - gridSize ; row += gridSize) {
-            for (int column = 0; column < im.getWidth() - gridSize; column += gridSize) {
-           Color AverageCol = ComputeAvgColor(im,row, gridSize, column);
-            sg.setColorAndTransparency(AverageCol, 1.0);
-        //  This is method to draw the dots
-            //  sg.drawDot(row, column, 1);
-            
-           sg.drawFilledBox(column, row, gridSize, gridSize);
-
+    public static void pixelateImage(DrwImage image, SimpleGUI simpleGui, int gridSize) {
+        for (int row = 0; row < image.getHeight() - gridSize; row += gridSize) {
+            for (int column = 0; column < image.getWidth() - gridSize; column += gridSize) {
+                Color AverageCol = ComputeAvgColor(image, row, gridSize, column);
+                simpleGui.setColorAndTransparency(AverageCol, 1.0);
+                simpleGui.drawFilledBox(column, row, gridSize, gridSize);
             }
 
         }
     }
 
     // method for average color
-    public static Color ComputeAvgColor(DrwImage im, int row, int gridSize, int column) {
+    public static Color ComputeAvgColor(DrwImage image, int row, int gridSize, int column) {
         int colorR = 0, colorG = 0, colorB = 0;
 
         //Loops for average color
-        for (int r = row; r < row+ gridSize -1  ; r = r + 1){
-        for (int c = column; c < column + gridSize -1; c = c + 1) {
-             
-                RGB rgb = im.getPixelRGB(c, r);
-              // calculating th total colr
+        for (int r = row; r < row + gridSize - 1; r = r + 1) {
+            for (int c = column; c < column + gridSize - 1; c = c + 1) {
+
+                RGB rgb = image.getPixelRGB(c, r);
+                // calculating th total colr
                 colorR += rgb.r;
                 colorG += rgb.g;
                 colorB += rgb.b;
             }
         }
         // getting the average of the color
-        int AvR = colorR /(gridSize * gridSize);
+        int AvR = colorR / (gridSize * gridSize);
         int AvB = colorB / (gridSize * gridSize);
-        int AvG = colorG /(gridSize * gridSize);
-        
-            return new Color(AvR, AvG, AvB);
+        int AvG = colorG / (gridSize * gridSize);
 
-    } 
+        return new Color(AvR, AvG, AvB);
+
+    }
 }
